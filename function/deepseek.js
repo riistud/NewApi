3473a4b8982cc1296dd0ef2ac4a21bdb913ade2d
RiiDEV <fakhri.ilham525@smp.belajar.id>
2025-03-24 16:52:08 +0700

    Update deepseek.js

--- function/deepseek.js
+++ function/deepseek.js
@@ -1,22 +1,49 @@
-const axios = require("axios")
+const Groq = require('groq-sdk');
+require("../settings.js")
 
-async function DeepSeek(prompt, isR1 = false) {
-  let model = isR1 ? 'deepseek-ai/DeepSeek-R1' : 'deepseek-ai/DeepSeek-V3'
+let api = [
+"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",
+" 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"
+]
 
-  try {
-    const response = await axios.post(
-      'https://api.blackbox.ai/api/chat',
-      {
-        messages: [{ content: prompt, role: "user" }],
-        model,
-        max_tokens: '1024',
-      },
-      { headers: { 'Content-Type': 'application/json' } }
-    )
+let apikey = api[Math.floor(Math.random() * api.length)]
 
-    return response.data.replace(/\*\*/g, '*').trim().split("</think>")[1].trim()
-  } catch (error) {
-    console.error(error)
+const client = new Groq({
+  apiKey: apikey,
+});
+
+async function groq(teks, prompt = `sekarang kamu adalah ai yang mengetahui tentang semua bahasa pemrograman dan selalu gunakan bahasa Indonesia saat menjawab semua pertanyaan!`) {
+try {
+  const chatCompletion = await client.chat.completions
+    .create({
+      messages: [
+        { role: 'system', content: prompt },
+        { role: 'user', content: teks }
+      ],
+      model: 'deepseek-r1-distill-qwen-32b',
+    })
+    .catch(async (err) => {
+      if (err instanceof Groq.APIError) {
+        console.log(err.status);
+        console.log(err.name);
+        console.log(err.headers);
+      } else {
+        throw err;
+      }
+    })
+
+    return {
+  status: true, 
+  creator: global.creator,
+  respon: chatCompletion.choices[0].message.content
+  }
+
+  } catch (e) {
+  return {
+  status: false, 
+  creator: global.creator,
+  respon: "Error :" + e  
+  }
   }
 }
 
