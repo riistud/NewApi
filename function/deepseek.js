aa03c5065031446f76e87a3430d3a3db74e92e98
RiiDEV <fakhri.ilham525@smp.belajar.id>
2025-03-24 16:59:40 +0700

    Update deepseek.js

--- function/deepseek.js
+++ function/deepseek.js
@@ -1,50 +1,31 @@
 const Groq = require('groq-sdk');
-require("../settings.js")
+require("../settings.js");
 
 let api = [
-"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",
-" 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"
-]
+  "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",
+  "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"
+];
 
-let apikey = api[Math.floor(Math.random() * api.length)]
+let apikey = api[Math.floor(Math.random() * api.length)];
 
 const client = new Groq({
   apiKey: apikey,
 });
 
-async function groq(teks, prompt = `sekarang kamu adalah ai yang mengetahui tentang semua bahasa pemrograman dan selalu gunakan bahasa Indonesia saat menjawab semua pertanyaan!`) {
-try {
-  const chatCompletion = await client.chat.completions
-    .create({
+async function DeepSeek(teks, prompt = "sekarang kamu adalah AI yang mengetahui tentang semua bahasa pemrograman dan selalu gunakan bahasa Indonesia saat menjawab semua pertanyaan!") {
+  try {
+    const chatCompletion = await client.chat.completions.create({
       messages: [
         { role: 'system', content: prompt },
         { role: 'user', content: teks }
       ],
       model: 'deepseek-r1-distill-qwen-32b',
-    })
-    .catch(async (err) => {
-      if (err instanceof Groq.APIError) {
-        console.log(err.status);
-        console.log(err.name);
-        console.log(err.headers);
-      } else {
-        throw err;
-      }
-    })
+    });
 
-    return {
-  status: true, 
-  creator: global.creator,
-  respon: chatCompletion.choices[0].message.content
-  }
-
+    return chatCompletion.choices[0].message.content;
   } catch (e) {
-  return {
-  status: false, 
-  creator: global.creator,
-  respon: "Error :" + e  
-  }
+    return `Error: ${e.message}`;
   }
 }
 
-module.exports = { DeepSeek }
+module.exports = { DeepSeek };
