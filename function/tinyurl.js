92cbcc5afe312c739e6942d8714423bd95a481ad
RiiDEV <fakhri.ilham525@smp.belajar.id>
2025-04-07 15:42:26 +0700

    Update tinyurl.js

--- function/tinyurl.js
+++ function/tinyurl.js
@@ -1,18 +1,46 @@
-const axios = require("axios")
+const axios = require("axios");
 
+// TinyURL Shortener
 async function shortUrl(links) {
-return new Promise(async (resolve, reject) => {
-try {
-var res = await axios.get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(links))
-resolve(res.data.toString())
-} catch (error) {
-reject(error)
-}
-})
+  try {
+    const res = await axios.get('https://tinyurl.com/api-create.php?url=' + encodeURIComponent(links));
+    return res.data.toString();
+  } catch (error) {
+    throw new Error("TinyURL Error: " + error.message);
+  }
 }
 
+// is.gd Shortener
 async function shortUrl2(links) {
-return axios.get(`https://is.gd/create.php?format=simple&url=${links}`).then(response => response.data)
+  try {
+    const res = await axios.get(`https://is.gd/create.php?format=simple&url=${encodeURIComponent(links)}`);
+    return res.data;
+  } catch (error) {
+    throw new Error("is.gd Error: " + error.message);
+  }
 }
 
-module.exports = { shortUrl, shortUrl2 }
+// SafelinkU Shortener
+async function safelinku(url, alias = "", passcode = "") {
+  try {
+    const response = await axios.post(
+      "https://safelinku.com/api/v1/links",
+      {
+        url: url,
+        alias: alias,
+        passcode: passcode
+      },
+      {
+        headers: {
+          Authorization: "Bearer 2955189a3b3916acaa24c32b3a1b0b897280266f",
+          "Content-Type": "application/json"
+        }
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error("SafelinkU Error: " + (error.response?.data?.message || error.message));
+  }
+}
+
+module.exports = { shortUrl, shortUrl2, safeLinku };
